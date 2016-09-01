833b8453b1e4d50c259904b84041a02fd868144c
billyfournier <billyfournier2000@yahoo.com>
2016-08-31 19:30:38 -0500

    eod commit

--- app/models/UserModel.py
+++ app/models/UserModel.py
@@ -10,5 +10,23 @@ class UserModel(Model):
             api_key = infile.readline().rstrip()
         # this comes from 'import requests'
         # can access the json data of this request through the 'text' attribute
-        api_response = requests.get('http://api.nal.usda.gov/ndb/reports/?ndbno=01009&type=b&format=json&api_key='+api_key).content
+        api_response = requests.get('http://api.nal.usda.gov/ndb/reports/?format=json&ndbno=01009&type=b&api_key='+api_key).content
+        return api_response
+
+    def get_food_data(self):
+        with open('.api_key') as infile:
+            api_key = infile.readline().rstrip()
+        api_query =  'http://api.nal.usda.gov/ndb/reports/'
+        api_query += '?type=b&format=json'
+        api_query += '&ndbno='    + '01009'
+        api_query += '&api_key='  + api_key
+        api_response = requests.get(api_query).content
+        return api_response
+
+    def get_food_list(self):
+        with open('.api_key') as infile:
+            api_key = infile.readline().rstrip()
+        api_query =  'http://api.nal.usda.gov/ndb/list?'
+        api_query += 'format=json&lt=f&max=1000'+api_key
+        api_response = requests.get(query)
         return api_response
